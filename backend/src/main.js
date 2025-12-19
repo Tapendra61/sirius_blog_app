13f45950cc7a2e91fe1bb092d410a95026235da4
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-12-19 15:18:15 +0545

    Using error middleware

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,16 +4,22 @@ import cookie_parser from "cookie-parser";
 import connect_to_db from "./config/db.js";
 import { PORT } from "./config/env.js";
 import auth_router from "./routes/auth.routes.js";
+import error_middleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
+// External Middlewares
 app.use(express.json());
 app.use(cookie_parser());
+
+// Internal Middlewares
+app.use(error_middleware);
 
 app.get("/", (req, res) => {
 	res.send("Express App!");
 });
 
+// Routes
 app.use("/api/v1/auth", auth_router);
 
 const start_server = async () => {
