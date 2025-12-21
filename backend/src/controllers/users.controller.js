eb3b033af9d6ec228798ee6ff66448ca12af0c11
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-12-21 10:56:53 +0545

    update_me controller.

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -28,22 +28,59 @@ export const me = async (req, res, next) => {
 export const update_me = async (req, res, next) => {
 	const { new_name, new_email, password } = req.body;
 	try {
-		const existing_user = await user_model.findById(req.user.user_id).select("+password");
+		if (!validator.isEmpty(new_email) && !validator.isEmail(new_email)) {
+			const error = new Error("Invalid email structure.");
+			error.status = 400;
+			throw error;
+		}
+
+		const user_with_new_email = await user_model.findOne({
+			email: new_email,
+		});
+		if (user_with_new_email) {
+			const error = new Error(
+				`A user with email: ${new_email} already exists. Pick another valid email.`,
+			);
+			error.status = 409;
+			throw error;
+		}
+
+		const existing_user = await user_model
+			.findById(req.user.user_id)
+			.select("+password");
 		if (!existing_user) {
 			const error = new Error("User not found!");
 			error.status = 404;
 			throw error;
 		}
-		
-		const password_matches = await bcrypt.compare(password, existing_user.password);
-		if(!password_matches){
+
+		const password_matches = await bcrypt.compare(
+			password,
+			existing_user.password,
+		);
+		if (!password_matches) {
 			const error = new Error("The password you entered is incorrect.");
 			error.status = 401;
 			throw error;
 		}
-		
-		
-		
+
+		if (!validator.isEmpty(new_name)) existing_user.name = new_name.trim();
+		if (!validator.isEmpty(new_email)) {
+			existing_user.email = new_email.trim();
+			existing_user.is_verified = false;
+		}
+
+		await existing_user.save();
+
+		res.status(200).json({
+			message: "success",
+			user: {
+				id: existing_user.id,
+				name: existing_user.name,
+				email: existing_user.email,
+				is_verified: existing_user.is_verified,
+			},
+		});
 	} catch (error) {
 		next(error);
 	}
